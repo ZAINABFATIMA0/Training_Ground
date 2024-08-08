42f74b3e507f61ac82002fbef62d2bbde534f060
Zainab Fatima <zfj959@gmail.com>
2024-08-08 15:19:10 +0500

    Final Commit

--- ecommerce/clothing_catalogue/urls.py
+++ ecommerce/clothing_catalogue/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('analytics/', views.index, name='produc_index'),
 ]
--- ecommerce/clothing_catalogue/views.py
+++ ecommerce/clothing_catalogue/views.py
@@ -20,3 +20,6 @@ def product_list(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'clothing_catalogue/product_detail.html', {'product': product})
+
+def index(request):
+    return render(request, 'clothing_catalogue/analytic.html')
